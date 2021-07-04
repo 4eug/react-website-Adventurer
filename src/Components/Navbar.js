f11da36165263204db4fce744efa7ad590ccbf85
4eug <debraheug@gmail.com>
2021-07-04 10:56:59 +0100

    Get Started

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className="Navbar">
+                <div className=".navbar-container">
+                    
+                    </div>   
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
